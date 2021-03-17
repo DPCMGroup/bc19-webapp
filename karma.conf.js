29528b201d5b5f918e7b2a4fd70a69fc75bae7e9
Damiano <38348465+ToldoDM@users.noreply.github.com>
2021-03-17 11:24:59 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,12 +25,8 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/untitled'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      type : 'lcov',
+      dir : 'coverage/'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
