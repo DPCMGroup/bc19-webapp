34249b96f313b8550fb83b92886a6f69a0db961c
Ivan <ivan.piacere@studenti.unipd.it>
2021-04-12 11:29:18 +0200

    aggiunto messaggio di errore

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,14 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
 
+  errorVisible: string;
+
   ngOnInit(): void {
+    this.setErrorVisible(false);
+  }
+
+  focused(): void {
+    this.setErrorVisible(false);
   }
 
   login(): void {
@@ -25,9 +32,51 @@ export class LoginComponent implements OnInit {
     this.service.login(credentials).subscribe( (data) => {
       if ( data.type === 0) {
         window.location.href = '/base';
+        this.setErrorVisible(false);
       }
+      this.setErrorVisible(true);
       console.log(data);
+
     });
+  }
+
+  loginServiceNoDB(credentials): Promise<any>{
+    const validCred =  {
+      username: 'mrossi',
+      password: '000',
+      type: 0
+    };
+    return new Promise<any>((resolve, reject) => {
+      setTimeout( () => {
+        if (credentials.username === validCred.username
+         && credentials.password === validCred.password) {
+          resolve(validCred);
+        }else{
+          resolve('No user found');
+        }
+      }, 100);
+    });
+  }
+
+  loginNoDB(): void {
+    const credentials = {
+      username: this.username,
+      password: this.password
+    };
+    this.loginServiceNoDB(credentials).then( (data) => {
+      if ( data.type === 0) {
+        window.location.href = '/base';
+        this.setErrorVisible(false);
+      }
+      this.setErrorVisible(true);
+      console.log(data);
+
+    });
+  }
+
+  setErrorVisible(b: boolean): void {
+    console.log('set error visible: ' + b);
+    this.errorVisible = b ? 'visible' : 'hidden';
   }
 
 }
